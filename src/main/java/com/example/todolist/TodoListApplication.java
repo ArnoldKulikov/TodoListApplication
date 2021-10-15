f16df9e42a386ef10f1accc71beab8596123b8b1
kulikov <arnold.kulikov@bcs-ideas.com>
2021-10-15 15:18:15 +0300

    - класс Task вынесен в отельный файл в пакет - System.exit(0) заменен на return - scanner заменен на BufferedReader - receivedTask переименован в command - удалены зависимости Spring boot - удален неиспользуемый файл с найтройками Spring boot

--- src/main/java/com/example/todolist/TodoListApplication.java
+++ src/main/java/com/example/todolist/TodoListApplication.java
@@ -1,14 +1,8 @@
 package com.example.todolist;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.invoke.SwitchPoint;
-import java.util.Scanner;
 
-@SpringBootApplication
 public class TodoListApplication {
 
 	public static void main(String[] args) {
