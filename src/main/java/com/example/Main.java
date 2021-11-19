9cdb8ed9dae67bdbd02267e218c3ad5b4469fae7
kulikov <arnold.kulikov@bcs-ideas.com>
2021-11-19 09:57:08 +0300

    - добавление SpringBoot в проект

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import com.example.core.TodoListApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         TodoListApplication.getInstance().run();
