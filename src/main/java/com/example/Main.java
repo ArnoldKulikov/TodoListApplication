a7bccaa5678e0a5ec9bd376fa037927a438d5940
kulikov <arnold.kulikov@bcs-ideas.com>
2021-11-27 17:46:07 +0300

    Добавление UI для свагера

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,10 +6,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @Component
 @RequiredArgsConstructor
+@EnableSwagger2
 public class Main implements CommandLineRunner {
 
     private final TodoListApplication todoListApplication;
