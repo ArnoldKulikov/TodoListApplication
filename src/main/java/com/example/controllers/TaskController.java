9811b8235d52b49388be41ff0c55731da1c74dfa
kulikov <arnold.kulikov@bcs-ideas.com>
2021-12-13 03:04:08 +0300

    Все методы поправлены, нужно протестировать

--- src/main/java/com/example/controllers/TaskController.java
+++ src/main/java/com/example/controllers/TaskController.java
@@ -1,30 +1,23 @@
 package com.example.controllers;
 
-import com.example.entities.Task;
 import com.example.exeption.MyException;
 import com.example.models.request.ChangeTaskRequestDto;
 import com.example.models.request.CreateTaskRequestDto;
 import com.example.models.response.TaskListResponseDto;
 import com.example.models.response.TaskResponseDto;
-import com.example.repositories.TaskRepository;
-import com.example.repositories.UserRepository;
 import com.example.services.TaskService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/task")
 public class TaskController {
 
-    private final TaskRepository taskRepository;
     private final TaskService taskService;
 
     @PostMapping
@@ -34,39 +27,30 @@ public class TaskController {
 
     @GetMapping
     public TaskListResponseDto getTaskList() {
-        return new TaskListResponseDto(taskRepository.findByClosedOrderByIdAsc(false));
+        return new TaskListResponseDto(taskService.getOpenTaskList());
     }
 
     @GetMapping("/all")
     public TaskListResponseDto getTaskListAll() {
-        List<Task> localTasks = taskRepository.findAllByOrderByIdAsc();
-        return new TaskListResponseDto(localTasks);
+        return new TaskListResponseDto(taskService.getTaskList());
     }
 
     @GetMapping("/search")
     public TaskListResponseDto getTaskListBySearch(
             @RequestParam("search") @NotNull String searchText
     ) {
-        return new TaskListResponseDto(taskRepository.findByDescriptionContainingOrderByIdAsc(searchText));
+        return new TaskListResponseDto(taskService.searchTask(searchText));
     }
 
     @PatchMapping("/{task_id}")
     public TaskResponseDto changeTask(
             @PathVariable("task_id") @NonNull Long taskId,
             @Valid @RequestBody ChangeTaskRequestDto task) throws MyException {
-        Optional<Task> result = taskRepository.findById(taskId);
-        if (result.isPresent()) {
-            Task localTask = result.get();
-            if (task.getClosed() != null) localTask.setClosed(task.getClosed());
-            if (task.getDescription() != null) localTask.setDescription(task.getDescription());
-            taskRepository.save(localTask);
-            return new TaskResponseDto(localTask);
-        }
-        throw new MyException("taskNotFound");
+        return new TaskResponseDto(taskService.changeTask(taskId, task.getClosed(), task.getDescription()));
     }
 
     @DeleteMapping("/{task_id}")
-    public void deleteTask(@PathVariable("task_id") @NonNull Long taskId) throws EmptyResultDataAccessException {
-        taskRepository.deleteById(taskId);
+    public void deleteTask(@PathVariable("task_id") @NonNull Long taskId) throws MyException {
+        taskService.deleteTask(taskId);
     }
 }
--- src/main/java/com/example/services/TaskService.java
+++ src/main/java/com/example/services/TaskService.java
@@ -2,9 +2,13 @@ package com.example.services;
 
 import com.example.entities.Task;
 import com.example.entities.User;
+import com.example.exeption.MyException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +22,47 @@ public class TaskService {
         Task localTask = taskServices(description);
         user.getTasks().add(localTask);
         return localTask;
+    }
+
+    public List<Task> getOpenTaskList() {
+        User user = userService.getCurrentUser();
+        return user.getTasks()
+                .stream()
+                .filter(task -> !task.getClosed())
+                .collect(Collectors.toList());
+    }
+
+    public List<Task> getTaskList() {
+        User user = userService.getCurrentUser();
+        return user.getTasks();
+    }
+
+    public List<Task> searchTask(String search) {
+        User user = userService.getCurrentUser();
+        return user.getTasks()
+                .stream()
+                .filter(task -> task.getDescription().contains(search))
+                .collect(Collectors.toList());
+    }
+
+    public Task changeTask(Long taskId, Boolean closed, String description) throws MyException {
+        User user = userService.getCurrentUser();
+        Task localTask = user.getTasks()
+                .stream()
+                .filter(task -> task.getId() == taskId)
+                .findFirst().orElseThrow(() -> new MyException("taskNotFound"));
+        if (closed != null) localTask.setClosed(closed);
+        if (description != null) localTask.setDescription(description);
+        return localTask;
+    }
+
+    public void deleteTask(Long taskId) throws MyException {
+        User user = userService.getCurrentUser();
+        Task localTask = user.getTasks()
+                .stream()
+                .filter(task -> task.getId() == taskId)
+                .findFirst().orElseThrow(() -> new MyException("taskNotFound"));
+        user.getTasks().remove(localTask);
     }
 
     private Task taskServices(String description) {
