6d6238c533cfcc6821c11e7418e778a674ab90c3
kulikov <arnold.kulikov@bcs-ideas.com>
2021-12-14 22:15:43 +0300

    Исправление замечаний

--- src/main/resources/db/init/11-12-2021-createUserRolesTable.sql
+++ src/main/resources/db/init/11-12-2021-createUserRolesTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users_roles
 (
-    users_id BIGINT NOT NULL,
-    roles_id BIGINT NOT NULL
+    users_id BIGINT REFERENCES users,
+    roles_id BIGINT REFERENCES roles
 
 );
